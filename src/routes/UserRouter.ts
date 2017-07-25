572a390fe510850ec363d05ad3b57da27862cf51
Christian Todd <christiantodd27@icloud.com>
2017-07-25 14:17:35 -0400

    add getOne functionality for users.

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -19,10 +19,31 @@ export class UserRouter {
   }
 
   /* 
+  GET one user by id
+  */
+  public getOne(req: Request, res: Response, next: NextFunction) {
+    let query = req.params.id;
+    let user = Users.find(hero => hero._id === query);
+    if (user) {
+      res.status(200).send({
+        message: 'Success!',
+        status: res.status,
+        user
+      });
+    } else {
+      res.status(404).send({
+        message: 'No user found with the supplied id.',
+        status: res.status
+      });
+    }
+  }
+
+  /* 
   * Take each handler, and attach to one of the Express Router's endpoints
   */
   init() {
     this.router.get('/', this.getAll);
+    this.router.get('/:id', this.getOne);
   }
 }
 
--- tests/users.test.ts
+++ tests/users.test.ts
@@ -16,4 +16,25 @@ describe('GET api/v1/users', () => {
       expect(res.body).to.have.length(5);
     });
   });
+  describe('GET api/v1/users/:id', () => {
+    it('should respond with a single JSON object ', () => {
+      return chai
+        .request(app)
+        .get('/api/v1/users/c23c1d4d-d8e2-456f-a126-b9525be87056')
+        .then(res => {
+          expect(res.status).to.equal(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.an('object');
+        });
+    });
+    it('should respond with Gudrun Friesen', () => {
+      return chai
+        .request(app)
+        .get('/api/v1/users/c23c1d4d-d8e2-456f-a126-b9525be87056')
+        .then(res => {
+          expect(res.body.user.profile.firstName).to.equal('Gudrun');
+          expect(res.body.user.profile.lastName).to.equal('Friesen');
+        });
+    });
+  });
 });
