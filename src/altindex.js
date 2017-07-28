92206c01f4b22bac41b08850f8f3c82d9dc0a9f6
Christian Todd <christiantodd27@icloud.com>
2017-07-28 00:23:18 -0400

    adjusting config.

--- src/altindex.js
+++ src/altindex.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 import Api from './Api';
 const mongoose = require('mongoose');
 
-const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL;
 const PORT = process.env.PORT || 3000;
 const JWT_SECRET = process.env.JWT_SECRET;
 
