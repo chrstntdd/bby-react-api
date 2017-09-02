edc60ee2c1292403df8c3a8d7fcfaa5c3656171e
Christian Todd <christiantodd27@icloud.com>
2017-09-02 11:36:48 -0400

    remove registration email functionality temporarily.

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -297,18 +297,18 @@ export default class UserRouter {
           `If you did not request this, please ignore this email.\n`
       };
       /* don't send a confirmation email when testing / development, but return the same result */
-      if (process.env.NODE_ENV === 'production') {
-        sgMail.send(emailData);
-        res.status(201).json({
-          message:
-            'Your account has been created, now please check your work email to confirm your account.'
-        });
-      } else {
-        res.status(201).json({
-          message:
-            'Your account has been created, now please check your work email to confirm your account.'
-        });
-      }
+      // if (process.env.NODE_ENV === 'production') {
+      //   sgMail.send(emailData);
+      //   res.status(201).json({
+      //     message:
+      //       'Your account has been created, now please check your work email to confirm your account.'
+      //   });
+      // } else {
+      res.status(201).json({
+        message:
+          'Your account has been created, now please check your work email to confirm your account.'
+      });
+      // }
     }
   }
 
